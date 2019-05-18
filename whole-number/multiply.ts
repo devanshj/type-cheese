66d6863785494ece5a0934d4f5f6d3b387546723
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2019-05-18 21:03:18 +0530

    Updates to have same name conventions

--- whole-number/multiply.ts
+++ whole-number/multiply.ts
@@ -2,10 +2,10 @@ import { TypeOf } from "../misc";
 import { _Add } from "./add";
 import { _Decrement } from "./decrement";
 
-export type __Multiply<X, Y> = 
+export type $Multiply<X, Y> = 
 	Y extends 1
 		? { type: X }
 		: { type: _Add<X, _Multiply<X, _Decrement<Y>>> };
 
-export type _Multiply<X, Y> = TypeOf<__Multiply<X, Y>>;
+export type _Multiply<X, Y> = TypeOf<$Multiply<X, Y>>;
 export type Multiply<X extends number, Y extends number> = _Multiply<X, Y>;
--- whole-number/pow.ts
+++ whole-number/pow.ts
@@ -3,10 +3,10 @@ import { _Multiply } from "./multiply";
 import { _Decrement } from "./decrement";
 
 
-export type __Pow<X, Y> = 
+export type $Pow<X, Y> = 
 	Y extends 1
 		? { type: X }
 		: { type: _Multiply<X, _Multiply<X, _Decrement<Y>>> };
 
-export type _Pow<X, Y> = TypeOf<__Pow<X, Y>>;
+export type _Pow<X, Y> = TypeOf<$Pow<X, Y>>;
 export type Pow<X extends number, Y extends number> = _Pow<X, Y>;
